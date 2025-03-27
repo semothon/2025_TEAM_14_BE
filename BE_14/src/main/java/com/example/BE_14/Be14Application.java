c57960b2a569902f81bea632065042bb4c4e255c
woo <samuel2626@khu.ac.kr>
2025-03-27 13:56:48 +0900

    백엔드 첫 세팅

---
+++ BE_14/src/main/java/com/example/BE_14/Be14Application.java
@@ -0,0 +1,13 @@
+package com.example.BE_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Be14Application.class, args);
+	}
+
+}
---
+++ BE_14/src/test/java/com/example/BE_14/Be14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
