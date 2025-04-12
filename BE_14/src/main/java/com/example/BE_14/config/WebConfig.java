dd963e146b05d90793402d6c87790dc6a50fdc69
woo <samuel2626@khu.ac.kr>
2025-04-12 16:34:46 +0900

    배포용 예시 코드 ngrok 사용

--- BE_14/src/main/java/com/example/BE_14/config/WebConfig.java
+++ BE_14/src/main/java/com/example/BE_14/config/WebConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// ngrok 설정, 프론트와 웹으로 연결, 무료, 프론트 서버 실행시 매번 사이트 바뀜
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React dev server
+                .allowedOrigins("https://51fd-61-98-214-250.ngrok-free.app") // React dev server, 프론트 서버 사이트랑 연동
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
